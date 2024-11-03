e0486826a4a2b921cbdb9fef152bf0878249fb54
newnonsick <thitivath.m@ku.th>
2024-11-03 19:53:30 +0700

    refactor chat repository interface and implementation; add GetMessageByID method for message retrieval

--- internal/repositories/interfaces/chat.go
+++ internal/repositories/interfaces/chat.go
@@ -8,23 +8,15 @@ import (
 )
 
 type ChatRepository interface {
-	GetChatMessageByID(ctx context.Context, chatID uuid.UUID, limit int, offset int) (*[]models.Message, error)
-
+	GetChatMessageByID(ctx context.Context, chatID uuid.UUID, limit int, offset int) (*[]models.Message, error) // Get messages of a chat
 	GetChatByID(ctx context.Context, chatID uuid.UUID) (*models.Chat, error)
-
 	IsUserPartOfChat(ctx context.Context, userID, chatID uuid.UUID) (bool, error)
-
 	SaveMessage(ctx context.Context, message *models.Message) (*models.Message, error)
-
 	CreateChat(ctx context.Context, chat *models.Chat) error
-
 	AddUserToChat(ctx context.Context, userID, chatID uuid.UUID) error
-
 	RemoveUserFromChat(ctx context.Context, userID, chatID uuid.UUID) error
-
 	UpdateChatMessage(ctx context.Context, message *models.Message) error
-
 	DeleteChatMessage(ctx context.Context, messageID uuid.UUID) error
-
 	UpdateChatMessageReadStatus(ctx context.Context, chatID uuid.UUID, userID uuid.UUID) error
+	GetMessageByID(ctx context.Context, messageID uuid.UUID) (*models.Message, error) // Get a message by ID
 }
